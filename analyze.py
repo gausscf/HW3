bb4572a9deeaf2690d6bac0ce498015288084a44
Aleksander Kubica <olek.kubica@gmail.com>
2015-10-27 23:41:50 -0700

    aaah

---
+++ analyze.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def analyze(filename):
+
+    data = np.loadtxt(fname=filename, delimiter=',')
+
+    fig = plt.figure(figsize=(10.0, 3.0))
+
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
+
+    axes1.set_ylabel('average')
+    axes1.plot(data.mean(axis=0))
+
+    axes2.set_ylabel('max')
+    axes2.plot(data.max(axis=0))
+
+    axes3.set_ylabel('min')
+    axes3.plot(data.min(axis=0))
+
+    # fig.tight_layout()
+    # plt.show(fig)
+    fig.savefig('plot.png')
+
+analyze('inflammation-01.csv')
